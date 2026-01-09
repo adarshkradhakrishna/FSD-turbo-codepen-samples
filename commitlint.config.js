ae72c35dd1317df6ef1bd19ba7cf31e3f9ef5c32
adarsh <adarsh.k@durucooperation.com>
2026-01-09 15:14:37 +0530

    chore(commitlint.config): changed the body-empty rule in configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,6 @@ module.exports = {
   "extends": ['@commitlint/config-conventional'],
   rules:{
     "header-max-length": [0, "always", 72], 
-    "body-empty":[2,"never"],
     'type-enum': [
       2,
       'always',
